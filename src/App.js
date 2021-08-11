d743ebe09fbe76cbee2b231cb4222e1f3a8f20b9
Prabhat Ranjan <prabhat5172992@gmail.com>
2021-08-11 14:16:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data] = useState(createSLB());
+
+  function createSLB() {
+    let rows = [];
+    let arr = [];
+
+    for (let i = 100, j = 1; i > 0; i--, j++) {
+      arr.push(i);
+      if (j % 10 === 0) {
+        if (rows.length % 2 === 0) {
+          rows.push(arr);
+        } else {
+          rows.push(arr.reverse());
+        }
+
+        arr = [];
+      }
+    }
+    return rows;
+  }
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sanke & Ladder board</h1>
+      <table className="outer_wrap"><tbody>
+        {data.map((item, i) => (
+          <tr key={`key_${i}`}>
+            {item.map((itm) => (
+              <td key={`item_${i+itm}`} className={itm%2 === 0 ? 'board-color' : 'board-white'}>{itm}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody></table>
     </div>
   );
 }
